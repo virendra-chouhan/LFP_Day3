d3ce1b9cd8d8e431348958c4272c4b2f03b653b3
virendra <virendrachouhan1494@gmail.com>
2022-07-28 15:24:06 +0530

    UC5_Implimented

--- Day3_Assignment/UC4_Emp_Wage/UC4_Emp_Wage_/UC4_Emp_Wage_/Program.cs
+++ Day3_Assignment/UC4_Emp_Wage/UC4_Emp_Wage_/UC4_Emp_Wage_/Program.cs
@@ -8,35 +8,42 @@ namespace UC4_Emp_Wage_
         public const int Is_Parttime = 2;
 
         public const int Emp_Hrs_Rate = 25;
-        
-        public const int Defoult = 0;
-        
+        public const int Num_of_Working_Days = 26;
+
+
+
         static void Main(string[] args)
         {
-            
-
-            Random random = new Random();
-
-            int emp_type = random.Next(0, 3);
             int Emp_Hours;
             int Emp_Wage;
-            switch (emp_type)
+            int Total_Emp_Wage=0;
+
+            for(int days=0; days < Num_of_Working_Days; days++) 
             {
-                case Is_Fulltime:
-                    Emp_Hours = 8;
-                    break;
-                case Is_Parttime:
-                    Emp_Hours = 4;
-                    break;
-                default:
-                    Emp_Hours = 0;
-                    break;
+
+                Random random = new Random();
+                int emp_type = random.Next(0, 3);
+
+                switch (emp_type)
+                {
+                    case Is_Fulltime:
+                        Emp_Hours = 8;
+                        break;
+                    case Is_Parttime:
+                        Emp_Hours = 4;
+                        break;
+                    default:
+                        Emp_Hours = 0;
+                        break;
+                }
+
+                Emp_Wage = Emp_Hours * Emp_Hrs_Rate;
+                Total_Emp_Wage = Emp_Wage + Total_Emp_Wage;
+
+                Console.WriteLine("Emp_Type:" + emp_type);
+                Console.WriteLine("Emp_Wage:" + Emp_Wage);
             }
-
-            Emp_Wage = Emp_Hours * Emp_Hrs_Rate;
-            Console.WriteLine("Emp_Type:" + emp_type);
-            Console.WriteLine("Emp_Wage:" + Emp_Wage);
-
+            Console.WriteLine("Total_Emp_Wage:" + Total_Emp_Wage);
         }
     }
 }
