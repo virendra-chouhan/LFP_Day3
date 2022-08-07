559b7d4185b7124d19f1027dc9b7fa79cd89d02a
virendra <virendrachouhan1494@gmail.com>
2022-08-07 12:40:24 +0530

    UC8_Impliment

--- Day3_Assignment/UC4_Emp_Wage/UC4_Emp_Wage_/UC4_Emp_Wage_/Emp_Wage.cs
+++ Day3_Assignment/UC4_Emp_Wage/UC4_Emp_Wage_/UC4_Emp_Wage_/Emp_Wage.cs
@@ -10,10 +10,8 @@ namespace UC4_Emp_Wage_
         public const int Is_Fulltime = 1;
         public const int Is_Parttime = 2;
 
-        public const int Emp_Hrs_Rate = 25;
-        public const int Num_of_Working_Days = 26;
-        public const int Max_Hrs_In_Month = 160;
-        public void Total_Wage()
+
+        public void MultipleCompnies(string company, int Emp_Hrs_Rate, int Num_of_Working_Days, int Max_Hrs_In_Month)
         {
             int Emp_Hours;
             int Emp_Wage;
@@ -21,6 +19,7 @@ namespace UC4_Emp_Wage_
             int Total_Emp_Hrs = 0;
             int Total_Working_Day = 0;
 
+             
             while (Total_Emp_Hrs <= Max_Hrs_In_Month && Total_Working_Day <= Num_of_Working_Days)
             {
                 Total_Working_Day++;
@@ -47,7 +46,7 @@ namespace UC4_Emp_Wage_
                 Console.WriteLine("Total_Working_Day:" + Total_Working_Day);
             }
             Emp_Wage = Total_Emp_Hrs * Emp_Hrs_Rate;
-            Console.WriteLine("Total_Emp_Wage:" + Emp_Wage);
+            Console.WriteLine("Total_Emp_Wage of Company:" + company + Emp_Wage);
         }
     }
 }
